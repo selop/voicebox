e56bfdc694aa490a58bfcbfb7678222ea6a78f73
Jamie Pine <ijamespine@me.com>
2026-01-25 20:07:02 -0800

    Update download links in constants.ts to use versioned filenames for consistency with release structure

--- landing/src/lib/constants.ts
+++ landing/src/lib/constants.ts
@@ -4,11 +4,11 @@ export const LATEST_VERSION = 'v0.1.0';
 
 export const DOWNLOAD_LINKS = {
   macArm:
-    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_aarch64-apple-darwin.dmg',
+    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_0.1.0_aarch64.dmg',
   macIntel:
-    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_x86_64-apple-darwin.dmg',
+    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_0.1.0_x64.dmg',
   windows:
-    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_x86_64-pc-windows-msvc.msi',
+    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_0.1.0_x64_en-US.msi',
   linux:
     'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_x86_64-unknown-linux-gnu.AppImage',
 } as const;
