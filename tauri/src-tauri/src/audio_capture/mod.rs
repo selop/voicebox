610f64c762653abb8fa40a34e438eb9ec263ffae
Jamie Pine <ijamespine@me.com>
2026-01-31 07:44:28 -0800

    fix linux compile

--- tauri/src-tauri/src/audio_capture/mod.rs
+++ tauri/src-tauri/src/audio_capture/mod.rs
@@ -2,11 +2,15 @@
 mod macos;
 #[cfg(target_os = "windows")]
 mod windows;
+#[cfg(target_os = "linux")]
+mod linux;
 
 #[cfg(target_os = "macos")]
 pub use macos::*;
 #[cfg(target_os = "windows")]
 pub use windows::*;
+#[cfg(target_os = "linux")]
+pub use linux::*;
 
 use std::sync::{Arc, Mutex};
 
